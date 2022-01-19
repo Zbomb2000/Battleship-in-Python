ec7fc0e0c23b3323d27c1124068083a355f1586e
ZBOMB <75494994+Zbomb2000@users.noreply.github.com>
2022-01-19 10:25:19 -0500

    Added attack grid

--- Fix.py
+++ Fix.py
@@ -66,6 +66,7 @@ def validatePoint(input_var):
 
 # Creates the grid
 main_grid = {"a1": 0, "a2": 0, "a3": 0, "b1": 0, "b2": 0, "b3": 0, "c1": 0, "c2": 0, "c3": 0}
+attack_grid = {"a1": 0, "a2": 0, "a3": 0, "b1": 0, "b2": 0, "b3": 0, "c1": 0, "c2": 0, "c3": 0}
 
 print("")
 print("")
